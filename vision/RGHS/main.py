058d1ccf999f5077b6d966ddce36c6b26df14723
msa <msas@tuta.io>
2021-08-10 22:00:58 +0300

    implemented image color enhancement modules

---
+++ vision/RGHS/main.py
@@ -0,0 +1,52 @@
+
+# encoding=utf-8
+import os
+import numpy as np
+import cv2
+import natsort
+
+from LabStretching import LABStretching
+from color_equalisation import RGB_equalisation
+from global_stretching_RGB import stretching
+from relativeglobalhistogramstretching import RelativeGHstretching
+
+np.seterr(over='ignore')
+if __name__ == '__main__':
+    pass
+
+
+# folder = "C:/Users/Administrator/Desktop/UnderwaterImageEnhancement/NonPhysical/RGHS"
+folder = "/home/msa/Projects/UUVs/saruca/sarucaController/vision/underwaterTests/utils/inputs/"
+path = folder + "/MOV00016"
+files = os.listdir(path)
+files =  natsort.natsorted(files)
+
+for i in range(len(files)):
+    file = files[i]
+    filepath = path + "/" + file
+    prefix = file.split('.')[0]
+    if os.path.isfile(filepath):
+        print('********    file   ********',file)
+        img = cv2.imread(folder +'/MOV00016/' + file)
+        img = cv2.resize(img, (640, 480))
+        # img = cv2.imread('InputImages/' + file)
+        # path = np.unicode(path, 'utf-8')
+        # img = cv2.imread('InputImages/' + file)
+        # img = cv2.imread(np.unicode('InputImages/' + file, 'utf-8'))
+
+        # print('img',img)
+
+        height = len(img)
+        width = len(img[0])
+        # sceneRadiance = RGB_equalisation(img)
+
+        sceneRadiance = img
+        # sceneRadiance = RelativeGHstretching(sceneRadiance, height, width)
+
+        sceneRadiance = stretching(sceneRadiance)
+
+
+        sceneRadiance = LABStretching(sceneRadiance)
+
+
+        cv2.imwrite('OutputImages/' + prefix + '_RGHS.jpg', sceneRadiance)
